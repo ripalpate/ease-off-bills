ef92f0604b159af36da34a2457ec5845f625fd7c
Ripal Patel <ripal212007@gmail.com>
2019-01-19 11:55:10 -0600

    setup done

---
+++ src/App/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <button className='btn btn-danger'>HELP ME</button>
+        <Button
+            tag="a"
+            color="success"
+            size="large"
+            href="http://reactstrap.github.io"
+            target="_blank"
+        >
+            View Reactstrap Docs
+        </Button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
