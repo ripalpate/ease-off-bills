4e076fa8e2ce6df1ba8cd9e653c08502afd77da4
Ripal Patel <ripal212007@gmail.com>
2019-01-19 12:31:58 -0600

    login works

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import authRequests from '../../helpers/data/authRequests';
 import './Auth.scss';
 
 class Auth extends React.Component {
+  authenticateUser = (e) => {
+    e.preventDefault();
+    authRequests.authenticate().then(() => {
+
+    }).catch(err => console.error(err));
+  }
+
   render() {
     return (
       <div className="Auth">
-        <button className="btn btn-danger">Login</button>
+        <button className="btn btn-danger" onClick={this.authenticateUser}>Login</button>
       </div>
     );
   }
