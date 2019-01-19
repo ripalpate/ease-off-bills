94e585ffd96379908feb323517d263b6acab72d7
Ripal Patel <ripal212007@gmail.com>
2019-01-19 12:40:42 -0600

    auth state is changing

--- src/App/App.js
+++ src/App/App.js
@@ -2,18 +2,34 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import connection from '../helpers/data/connection';
 import Auth from '../components/Auth/Auth';
+import Bills from '../components/Bills/Bills';
 
 import './App.scss';
 
 class App extends Component {
+  state = {
+    authed: false,
+  }
+
   componentDidMount() {
     connection();
   }
 
+  isAuthenticated = () => {
+    this.setState({ authed: true });
+  }
+
   render() {
+    if  (!this.state.authed) {
+      return (
+        <div className="App">
+          <Auth isAuthenticated={this.isAuthenticated}/>
+        </div>
+      );
+    }
     return (
       <div className="App">
-        <Auth/>
+        <Bills/>
       </div>
     );
   }
---
+++ src/components/Bills/Bills.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Bills.scss';
+
+class Bills extends React.Component {
+  render() {
+    return (
+    <div>
+      <h3> Bills </h3>
+    </div>
+    );
+  }
+}
+
+export default Bills;
