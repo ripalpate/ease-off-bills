8c4101c1af961c847212053699b9bd212fa48168
Ripal Patel <ripal212007@gmail.com>
2019-01-19 21:34:28 -0600

    login and logout works

---
+++ src/helpers/apiKeys.js
@@ -0,0 +1,12 @@
+const apiKeys = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyCqIcde8rr5MsICJu5V0veBIMN9rRFFZ1Y',
+    authDomain: 'ease-off-bills.firebaseapp.com',
+    databaseURL: 'https://ease-off-bills.firebaseio.com',
+    projectId: 'ease-off-bills',
+    storageBucket: 'ease-off-bills.appspot.com',
+    messagingSenderId: '153389097962',
+  },
+};
+
+export default apiKeys;
