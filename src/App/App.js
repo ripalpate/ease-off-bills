2fff8f53695603e05b1b4a762cc1f8954cdbc6ce
Ripal Patel <ripal212007@gmail.com>
2019-01-20 13:50:24 -0600

    routing is working

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import firebase from 'firebase';
 import 'firebase/auth';
+import {
+  BrowserRouter, Route, Redirect, Switch,
+} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import connection from '../helpers/data/connection';
 import authRequests from '../helpers/data/authRequests';
-import Auth from '../components/Auth/Auth';
+import Auth from '../components/pages/Auth/Auth';
 import MyNavbar from '../components/MyNavbar/MyNavbar';
+import Bills from '../components/pages/Bills/Bills';
 import './App.scss';
 
-class App extends Component {
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = props => (authed === false
+    ? (<Component {...props}/>)
+    : (<Redirect to={{ pathname: '/bills', state: { from: props.location } }}/>));
+  return <Route {...rest} render={props => routeChecker(props)}/>;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = props => (authed === true
+    ? (<Component {...props}/>)
+    : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }}/>));
+  return <Route {...rest} render={props => routeChecker(props)}/>;
+};
+
+class App extends React.Component {
   state= {
     authed: false,
   }
@@ -42,17 +60,23 @@ class App extends Component {
       this.setState({ authed: false });
     };
 
-    if (!this.state.authed) {
-      return (
-        <div className="App">
-        <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent}/>
-        <Auth isAuthenticated={this.isAuthenticated}/>
-        </div>
-      );
-    }
     return (
       <div className="App">
-        <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent}/>
+        <BrowserRouter>
+          <React.Fragment>
+          <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent}/>
+          <div className="appContainer">
+            <div className="row">
+              <Switch>
+                <PrivateRoute exact path="/" component={Bills} authed={this.state.authed}/>
+                <PrivateRoute path="/bills" component={Bills} authed={this.state.authed}/>
+                <PublicRoute path="/auth" component={Auth} authed={this.state.authed}/>
+              </Switch>
+
+            </div>
+          </div>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/components/pages/Bills/Bills.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Bills.scss';
+
+class Bills extends React.Component {
+  render() {
+    return (
+      <div>
+        Bill Page
+      </div>
+    );
+  }
+}
+
+export default Bills;
