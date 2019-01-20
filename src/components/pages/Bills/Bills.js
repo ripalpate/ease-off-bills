257e1f198f377c55fbf8bb60b28674662c6fe51b
Ripal Patel <ripal212007@gmail.com>
2019-01-20 15:02:09 -0600

    Add Bill button added

--- src/components/pages/Bills/Bills.js
+++ src/components/pages/Bills/Bills.js
@@ -3,15 +3,24 @@ import { Button } from 'reactstrap';
 import './Bills.scss';
 
 class Bills extends React.Component {
-  changeView = (e) => {
+  changeToEditView = (e) => {
     const view = e.currentTarget.id;
     this.props.history.push(`/bills/:${view}/edit`);
   }
 
+  changeView = () => {
+    this.props.history.push('/bills/new');
+  }
+
   render() {
     return (
-        <div className="Bill mx-auto" id="1234" to="/bills/:id/edit" onClick={this.changeView}>
-        <Button className ="btn btn-info mt-5">Edit Bill</Button>
+      <div>
+        <div className="Bill mx-auto" id="1234" to="/bills/:id/edit" onClick={this.changeToEditView}>
+          <Button className ="btn btn-info mt-5">Edit Bill</Button>
+        </div>
+        <div className="Bill mx-auto" onClick={this.changeView}>
+          <Button className ="btn btn-info mt-5">Add Bills</Button>
+        </div>
       </div>
     );
   }
