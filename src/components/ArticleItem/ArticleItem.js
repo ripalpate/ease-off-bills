5fed61e6a85dabe1d52d507020488b92bb8e99ca
Ripal Patel <ripal212007@gmail.com>
2019-01-20 21:45:54 -0600

    articles are printing

---
+++ src/components/ArticleItem/ArticleItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import articleShape from '../../helpers/propz/articlesShape';
+import './ArticleItem.scss';
+
+class ArticleItem extends React.Component {
+  static propTypes = {
+    article: articleShape,
+  }
+
+  render() {
+    const { article } = this.props;
+    return (
+      <div className="card border-secondary">
+        <div className="card-body">
+        <h5 className="card-title">{article.title}</h5>
+        <p className="card-text">{article.synopsis}</p>
+        <a href={article.url} target="_blank" rel="noopener noreferrer" className="mr-3">Read more</a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ArticleItem;
--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import articleShape from '../../helpers/propz/articlesShape';
+import ArticleItem from '../ArticleItem/ArticleItem';
 import './Articles.scss';
 
 class Articles extends React.Component {
+  static propTypes = {
+    articles: PropTypes.arrayOf(articleShape),
+  }
+
   render() {
+    const { articles } = this.props;
+    const articlesItemComponents = articles.map(article => (
+      <ArticleItem
+      article={article}
+      key={article.id}
+      />
+    ));
     return (
-      <div>
+      <div className="col">
         <h5>Articles</h5>
+        {articlesItemComponents}
       </div>
     );
   }
