22affcc3d8033d223b2b0377ca4720dec9550a6e
Ripal Patel <ripal212007@gmail.com>
2019-01-21 19:31:30 -0600

    delete button is displaying

--- src/components/DueBillItem/DueBillItem.js
+++ src/components/DueBillItem/DueBillItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import billShape from '../../helpers/propz/billShape';
+import formatPrice from '../../helpers/formatPrice';
 import './DueBillItem.scss';
 
 class DueBillItem extends React.Component {
@@ -9,14 +10,44 @@ class DueBillItem extends React.Component {
 
   render() {
     const { bill } = this.props;
+    // const makeButtons = () => (
+    //       <div>
+    //         <span className="col">
+    //           <button className="btn btn-default">
+    //             <i className="fas fa-trash-alt"></i>
+    //           </button>
+    //         </span>
+    //       </div>
+    // );
+    const dueBillElement = () => {
+      if (!bill.isPaid) {
+        return (
+          <div className="row">
+            <p className="col-2">{bill.dueDate}</p>
+            <p className="col-2">{bill.category}</p>
+            <p className="col-2">{formatPrice(bill.amount)}</p>
+            <p className="col-1"><a href={bill.paymentUrl} rel="noopener noreferrer" target="_blank">Pay</a></p>
+            <span className="col-2">
+            <input className=""type="checkbox"/>
+            <label className="checkbox-label">Paid</label></span>
+            <span className="col-1">
+              <button className="btn btn-danger delete-button">
+                <i className="fas fa-trash-alt"></i>
+              </button>
+            </span>
+            <span className="col-1">
+              <button className="btn btn-default delete-button">
+                <i className="fas fa-pencil-alt"></i>
+              </button>
+            </span>
+          </div>
+        );
+      }
+      return (<span></span>);
+    };
     return (
-      <div className="row">
-        <p className="col-2">{bill.dueDate}</p>
-        <p className="col-2">{bill.category}</p>
-        <p className="col-2">{bill.amount}</p>
-        <p className="col-2"><a href={bill.paymentUrl} rel="noopener noreferrer" target="_blank">Pay</a></p>
-        <input className=""type="checkbox"/>
-        <label className="checkbox-label">Paid</label>
+      <div>
+        {dueBillElement()}
       </div>
     );
   }
