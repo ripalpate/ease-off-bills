c8273bb83d305f9eba9f77620e7b9d346f289938
Ripal Patel <ripal212007@gmail.com>
2019-01-23 18:19:41 -0600

    cycle is working

--- src/components/pages/NewBill/NewBill.js
+++ src/components/pages/NewBill/NewBill.js
@@ -61,13 +61,38 @@ class NewBill extends React.Component {
       }).catch(err => console.error(err));
   }
 
+  // onChangeNumber = (e) => {
+  //   const x = e.currentTarget.value;
+  //   this.setState({ x });
+  // }
+
   formSubmit = (e) => {
     e.preventDefault();
     const myBill = { ...this.state.newBill };
     myBill.dueDate = Date.parse(`${this.state.billDueDate}T00:00:00`);
     myBill.uid = authRequests.getCurrentUid();
+    this.repeatCycle(myBill);
     this.formSubmitEvent(myBill);
     this.setState({ newBill: defaultBill, billDueDate: defaultDueDate });
+  }
+
+  repeatCycle = (myBill) => {
+    const occurances = document.getElementById('occurrences').value;
+    const cycle = document.getElementById('cycle').value;
+    let newDate;
+    for (let i = 1; i <= occurances - 1; i += 1) {
+      if (cycle === 'monthly') {
+        newDate = Date.parse(moment(myBill.dueDate).add(i, 'months').calendar());
+        const newBill = Object.assign({}, myBill);
+        newBill.dueDate = newDate;
+        this.formSubmitEvent(newBill);
+      } else if (cycle === 'yearly') {
+        newDate = Date.parse(moment(myBill.dueDate).add(i, 'years').calendar());
+        const newBill = Object.assign({}, myBill);
+        newBill.dueDate = newDate;
+        this.formSubmitEvent(newBill);
+      }
+    }
   }
 
   onChangeDisplayOccurances = (e) => {
@@ -146,9 +171,9 @@ class NewBill extends React.Component {
             </select>
           </div>
           <div className="form-group" id="showMe">
-            <label htmlFor="noOfOccurances">No. of Ocuurances: </label>
-            <select id="occurances"className="form-control">
-              <option>1</option>
+            <label htmlFor="noOfOccurances">No. of Occurrences: </label>
+            <select id="occurrences" className="form-control" onChange={this.onChangeNumber}>
+              <option value="1">1</option>
               <option value='2'>2</option>
               <option value='3'>3</option>
               <option value='4'>4</option>
