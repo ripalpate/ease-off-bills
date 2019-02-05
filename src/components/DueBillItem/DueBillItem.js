cad15fe8c810d3034d343071054b6c2816d93608
Ripal Patel <ripal212007@gmail.com>
2019-02-05 10:39:33 -0600

    added modal for due bills additional information

--- src/components/DueBillItem/DueBillItem.js
+++ src/components/DueBillItem/DueBillItem.js
@@ -2,8 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import {
-  Label, Input, FormGroup,
-  Button, Modal, ModalHeader, ModalBody,
+  Label,
+  Input,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
   ModalFooter,
 } from 'reactstrap';
 import billShape from '../../helpers/propz/billShape';
@@ -11,13 +15,8 @@ import formatPrice from '../../helpers/formatPrice';
 import './DueBillItem.scss';
 
 class DueBillItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal: false,
-    };
-
-    this.toggle = this.toggle.bind(this);
+  state = {
+    modal: false,
   }
 
   toggle() {
@@ -25,7 +24,6 @@ class DueBillItem extends React.Component {
       modal: !this.state.modal,
     });
   }
-
 
   static propTypes = {
     bill: billShape,
@@ -60,6 +58,7 @@ class DueBillItem extends React.Component {
   }
 
   render() {
+    const { modal } = this.state;
     const { bill } = this.props;
     const divStyleDanger = {
       backgroundColor: 'red',
@@ -95,13 +94,38 @@ class DueBillItem extends React.Component {
       return (
         <Label check onClick={this.deleteEvent}>
             <Input type="checkbox" />{' '}
-            Delete Series
+            Delete the Series of this bill
         </Label>
       );
     };
+
+    const modalElement = () => (
+      <Modal isOpen={modal} toggle={e => this.toggle(e)} className={this.props.className}>
+          <ModalHeader toggle={e => this.toggle(e)}>Detail Bill Information</ModalHeader>
+          <ModalBody>
+              <p className="col-sm pt-1 date-element">Due Date: {moment(bill.dueDate).format('L')}</p>
+              <p className="col-sm pt-1">Payee: {bill.payee}</p>
+              <p className="col-sm pt-1">Category: {bill.category}</p>
+              <p className="col-sm pt-1">Amount: {formatPrice(bill.amount)}</p>
+              <div className="buttons text-center">
+                <button className="btn btn-danger delete-button" title="Delete Bill" onClick={this.deleteSingleEvent}>
+                  <i className="fas fa-trash-alt"></i>
+                </button>
+                <button className="btn btn-secondary edit-button  ml-2" onClick={this.editEvent}>
+                  <i className="fas fa-pencil-alt"></i>
+                </button>
+                <p className="pl-3 m-3">{deleteSeriesButton()}</p>
+              </div>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="info" onClick={e => this.toggle(e)}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+    );
+
     const dueBillElement = () => (
       <div>
-        <div className="row single-bill mb-1" onClick={this.toggle}> {this.props.buttonLabel}
+        <div className="row single-bill mb-1" onClick={e => this.toggle(e)}> {this.props.buttonLabel}
             <div className="col-sm pt-1 date-element">{moment(bill.dueDate).format('L')}<p>{dueDays()}</p></div>
               <p className="col-sm pt-1">{bill.category}</p>
               <p className="col-sm pt-1">{formatPrice(bill.amount)}</p>
@@ -109,48 +133,9 @@ class DueBillItem extends React.Component {
             <span className="col-sm pt-1">
               <input className="paid-checkbox" type="checkbox" checked={bill.isPaid} onChange={this.updateIsPaidEvent}/>
               <label className="checkbox-label">Paid</label></span>
-              {/* <Button id="Popover1" className="col-sm" onClick={this.toggle}>
-                ... {this.props.buttonLabel}
-              </Button> */}
           </div>
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle}>Detail Bill Information</ModalHeader>
-          <ModalBody>
-              <p className="col-sm pt-1 date-element">Due Date: {moment(bill.dueDate).format('L')}</p>
-              <p className="col-sm pt-1">Payee: {bill.payee}</p>
-              <p className="col-sm pt-1">Category: {bill.category}</p>
-              <p className="col-sm pt-1">Amount: {formatPrice(bill.amount)}</p>
-              <button className="btn btn-danger delete-button ml-2" title="Delete Bill" onClick={this.deleteSingleEvent}>
-                <i className="fas fa-trash-alt"></i>
-              </button>
-              <button className="btn btn-default edit-button  ml-1" onClick={this.editEvent}>
-                <i className="fas fa-pencil-alt"></i>
-              </button>
-              <span className="pl-3 m-2">  {deleteSeriesButton()}</span>
-          </ModalBody>
-          <ModalFooter>
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
+          {modalElement()}
     </div>
-      // <div className="row single-bill">
-      //   <div className="col-sm pt-1 date-element">{moment(bill.dueDate).format('L')}<p>{dueDays()}</p></div>
-      //   <p className="col-sm pt-1">{bill.category}</p>
-      //   <p className="col-sm pt-1">{formatPrice(bill.amount)}</p>
-      //   <p className="col-sm pt-1"><a href={bill.paymentUrl} rel="noopener noreferrer" target="_blank">Pay</a></p>
-      //   <span className="col-sm pt-1">
-      //   <input className="paid-checkbox" type="checkbox" checked={bill.isPaid} onChange={this.updateIsPaidEvent}/>
-      //   <label className="checkbox-label">Paid</label></span>
-      //   <span className="col">
-      //     <button className="btn btn-danger delete-button" title="Delete Bill" onClick={this.deleteSingleEvent}>
-      //       <i className="fas fa-trash-alt"></i>
-      //     </button>
-      //     <button className="btn btn-default edit-button" onClick={this.editEvent}>
-      //       <i className="fas fa-pencil-alt"></i>
-      //     </button>
-      //     <small>  {deleteSeriesButton()}</small>
-      //   </span>
-      // </div>
     );
 
     return (
