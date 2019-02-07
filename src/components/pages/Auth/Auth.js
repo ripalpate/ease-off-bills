a3dfe0dd2fc2669d85b58601b587dce3c3e27760
Ripal Patel <ripal212007@gmail.com>
2019-02-06 22:25:18 -0600

    style for auth page

--- src/components/pages/Auth/Auth.js
+++ src/components/pages/Auth/Auth.js
@@ -27,6 +27,13 @@ class Auth extends React.Component {
   render() {
     return (
       <div className="Auth">
+        <div className="text-center mb-3">
+          <img src="https://www.myboost.com.my/wp-content/uploads/2018/08/paybills-banner2.png" width="250px" alt="important to pay bills"/>
+          {/* <img src="https://pngimage.net/wp-content/uploads/2018/06/png-gas-payment-6.png" width="250px" alt="important to pay bills"/> */}
+        </div>
+        <div className="text-center mb-3">
+          <img src="http://www.villageofjonescreektexas.com/yahoo_site_admin/assets/images/click-here-png-click-here-500.78134131_std.png" width="100px" alt="important to pay bills"/>
+        </div>
         <div>
           <button className="btn login-btn" onClick={this.authenticateUser}>
             <img src= {googleButton} alt="google login Button" width="400px"/>
